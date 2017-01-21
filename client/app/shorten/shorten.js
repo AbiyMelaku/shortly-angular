97f122eecb70c000319633a60c0af9563b486747
Abiy Melaku <agirma08@gmail.com>
2017-01-20 20:00:21 -0800

    wrote shorten controller
    bare bones html that doesn't work

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,17 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.link = {};
+  
+  $scope.addLink = function() {
+    $scope.loading = true;
+    Links.addOne($scope.link)
+    .then(function() {
+      $scope.loading = false;
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
+  }
+  
 });

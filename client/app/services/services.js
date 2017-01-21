72e4fa926c95d1d901471dbb02294fed20a835b6
Abiy Melaku <agirma08@gmail.com>
2017-01-20 17:50:50 -0800

    wrote link factories.  passed tests

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,7 +2,38 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getAll = function(links) {
+    return $http({
+      method: 'GET',
+      url: '/api/links',
+      data: links
+    })
+    .then(function (res) {
+      var resArray = res.data;
+      var linksArray = [];
+      resArray.forEach(function(obj) {
+        linksArray.push(obj);
+      });
+      return linksArray;
+    });
+  };
+  
+  var addOne = function(link) {
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: link
+    })
+  
+  };
+  
+  
+  return{
+    getAll: getAll,
+    addOne: addOne
+  };
 })
+
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user

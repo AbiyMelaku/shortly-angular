690fa8540f60466cb95e8d981baf1b56434c805d
Abiy Melaku <agirma08@gmail.com>
2017-01-20 19:52:56 -0800

    build links controller
    also added a small template

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,16 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+  var doSomethingToLinks = function() {
+    Links.getAll()
+    .then(function(links) {
+      $scope.data.links = links;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+    
+  }
+  doSomethingToLinks();
 });
